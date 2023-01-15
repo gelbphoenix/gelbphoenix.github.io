36f99e00496a7c82b1983ef066b1705d3378b7e7
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-15 23:38:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function navi() {
+    const nav = document.getElementById('mobil');
+    const menubutton = document.getElementById('menubutton');
+
+    if (nav.style.display == 'block') {
+        nav.style.display = 'none';
+        menubutton.innerHTML='menu';
+    } else {
+        nav.style.display = 'block';
+        menubutton.innerHTML='clear';
+    };
+}
