5dcc43994740f29770ef2c2234991ba0b9ec2bc7
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-16 21:16:31 +0100

    js update

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById('menubutton').addEventListener("click", navi)
+document.getElementById('menubutton').addEventListener("click", navi);
+document.getElementById('button-pol').addEventListener("click", polInfo);
+document.getElementById('button-cod').addEventListener("click", codeInfo);
+document.getElementById('button-gen').addEventListener("click", genInfo);
 
 function navi() {
     const nav = document.getElementById('mobil');
@@ -10,5 +13,44 @@ function navi() {
     } else {
         nav.style.display = 'flex';
         menubutton.innerHTML='clear';
+    };
+}
+
+function polInfo() {
+    const poli = document.getElementById('politic-infos');
+    const pbutton = document.getElementById('button-pol');
+
+    if (poli.style.display == 'flex') {
+        poli.style.display = 'none';
+        pbutton.style.display = 'flex';
+    } else {
+        poli.style.display = 'flex';
+        pbutton.style.display = 'none';
+    };
+}
+
+function codeInfo() {
+    const cod = document.getElementById('code-infos');
+    const cbutton = document.getElementById('button-cod');
+
+    if (cod.style.display == 'flex') {
+        cod.style.display = 'none';
+        cbutton.style.display = 'flex';
+    } else {
+        cod.style.display = 'flex';
+        cbutton.style.display = 'none';
+    };
+}
+
+function genInfo() {
+    const gen = document.getElementById('general-infos');
+    const gbutton = document.getElementById('button-gen');
+
+    if (gen.style.display == 'flex') {
+        gen.style.display = 'none';
+        gbutton.style.display = 'flex';
+    } else {
+        gen.style.display = 'flex';
+        gbutton.style.display = 'none';
     };
 }
