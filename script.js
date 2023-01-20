263a4828662c036d9973579e76ebef7dd98dce11
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-20 12:46:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-document.getElementById('menubutton').addEventListener("click", navi);
-document.getElementById('button-pol').addEventListener("click", polInfo);
-document.getElementById('button-cod').addEventListener("click", codeInfo);
-document.getElementById('button-gen').addEventListener("click", genInfo);
+document.getElementById('menubutton').addEventListener("click", toggleDisplay.bind(null, 'mobil'));
+document.getElementById('button-pol').addEventListener("click", toggleDisplay.bind(null, 'politic-infos'));
+document.getElementById('button-cod').addEventListener("click", toggleDisplay.bind(null, 'code-infos'));
+document.getElementById('button-gen').addEventListener("click", toggleDisplay.bind(null, 'general-infos'));
 
-// Funktion for the Dropnav
-function navi() {
-    const nav = document.getElementById('mobil');
-    const menubutton = document.getElementById('menubutton');
-
-    if (nav.style.display == 'flex') {
-        nav.style.display = 'none';
-        menubutton.src='./index/icons/menu.png';
+function toggleDisplay(id) {
+    const element = document.getElementById(id);
+    if (element.style.display === "flex") {
+        element.style.display = "none";
+        if (id === "mobil") {
+          const mb = document.getElementById('menubutton')
+          mb.src='./index/icons/menu.png';
+        }
     } else {
-        nav.style.display = 'flex';
-        menubutton.src='./index/icons/close.png';
-    };
-}
-
-// Function for Politik info
-function polInfo() {
-    const poli = document.getElementById('politic-infos');
-    const pbutton = document.getElementById('button-pol');
-
-    if (poli.style.display == 'flex') {
-        poli.style.display = 'none';
-    } else {
-        poli.style.display = 'flex';
-    };
-}
-
-// Function for coding info
-function codeInfo() {
-    const cod = document.getElementById('code-infos');
-    const cbutton = document.getElementById('button-cod');
-
-    if (cod.style.display == 'flex') {
-        cod.style.display = 'none';
-    } else {
-        cod.style.display = 'flex';;
-    };
-}
-
-// Function for general info
-function genInfo() {
-    const gen = document.getElementById('general-infos');
-    const gbutton = document.getElementById('button-gen');
-
-    if (gen.style.display == 'flex') {
-        gen.style.display = 'none';
-    } else {
-        gen.style.display = 'flex';
-    };
+        element.style.display = "flex";
+        if (id === "mobil")  {
+          const mb = document.getElementById('menubutton')
+          mb.src='./index/icons/close.png';
+        }
+    }
 }
