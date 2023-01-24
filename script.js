e51160ba005e173f5d27e3b0d3a2997b30b914da
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-24 23:46:10 +0100

    year mechanism updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", getBackground());
+document.addEventListener("DOMContentLoaded", getCrYear());
 document.getElementById('menubutton').addEventListener("click", toggleDisplay.bind(null, 'mobil'));
 document.getElementById('button-pol').addEventListener("click", toggleDisplay.bind(null, 'politic-infos'));
 document.getElementById('button-cod').addEventListener("click", toggleDisplay.bind(null, 'code-infos'));
@@ -15,6 +16,12 @@ function getBackground() {
   document.querySelector("html").style.background = "url(/index/background/" + chosenBackground +") no-repeat center fixed";
   document.querySelector("html").style.backgroundSize = "cover";
   document.querySelector("html").style.zIndex = "0";
+}
+
+function getCrYear() {
+  const date = new Date();
+  const dateyear = date.getFullYear();
+  document.getElementById('year').innerHTML = "©" + dateyear;
 }
 
 function toggleDisplay(id) {
