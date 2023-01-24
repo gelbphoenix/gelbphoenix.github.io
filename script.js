e9ad4f47b568e3a884fb36c1cae3e6881af858a2
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-24 17:51:13 +0100

    adding background

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ document.getElementById('button-gen').addEventListener("click", toggleDisplay.bi
 function getBackground() {
   let backgrounds = [
     "background.jpg",
-    "background2.jpg"
+    "background2.jpg",
+    "background3.jpg"
   ];
 
   let randomIndex = Math.floor(Math.random() * backgrounds.length);
 
   const chosenBackground = backgrounds[randomIndex];
 
-  document.querySelector("html").style.background = "url(/index/" + chosenBackground +") no-repeat center fixed";
+  document.querySelector("html").style.background = "url(/index/background/" + chosenBackground +") no-repeat center fixed";
   document.querySelector("html").style.backgroundSize = "cover";
   document.querySelector("html").style.zIndex = "0";
 }
