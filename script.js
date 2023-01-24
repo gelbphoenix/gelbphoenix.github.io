3f14c467e8ea9c1e1f03621bfc126f27dcf0f7c0
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-24 17:42:56 +0100

    changing Background img

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+document.addEventListener("DOMContentLoaded", getBackground());
 document.getElementById('menubutton').addEventListener("click", toggleDisplay.bind(null, 'mobil'));
 document.getElementById('button-pol').addEventListener("click", toggleDisplay.bind(null, 'politic-infos'));
 document.getElementById('button-cod').addEventListener("click", toggleDisplay.bind(null, 'code-infos'));
 document.getElementById('button-gen').addEventListener("click", toggleDisplay.bind(null, 'general-infos'));
+
+function getBackground() {
+  let backgrounds = [
+    "background.jpg",
+    "background2.jpg"
+  ];
+
+  let randomIndex = Math.floor(Math.random() * backgrounds.length);
+
+  const chosenBackground = backgrounds[randomIndex];
+
+  document.querySelector("html").style.background = "url(/index/" + chosenBackground +") no-repeat center fixed";
+  document.querySelector("html").style.backgroundSize = "cover";
+  document.querySelector("html").style.zIndex = "0";
+}
 
 function toggleDisplay(id) {
     const element = document.getElementById(id);
