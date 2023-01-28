67f5e1259bec0467c43fbec6da9d0aeb2e52ed3b
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-28 23:56:59 +0100

    fix

--- frontend/static/views/projects.js
+++ frontend/static/views/projects.js
@@ -38,12 +38,19 @@ export default class extends AbstractView {
                         Projekte:
                     </h3>
                 </div>
-                <div class="content-bar">
+                <div class="content-bar projects-bar">
                     <div class="content-box">
-                        <h4 style="margin-block-start: 0.5rem; margin-block-end: 0;">Diese Webpage</h4>
+                        <h4 style="margin-block-start: 0.5rem; margin-block-end: 0;">Diese Webpage (Version 1.0)</h4>
+                        <img src="/frontend/index/favicon.png" style="width: 2.5rem;">
+                        <p style="margin-block-start: .2rem;">Diese Web&#173;page wurde von mir zu 100% selber geschrieben und gestaltet.</p>
+                        <a href=https://old.gelbphoenix.de/ class="box-botton" target="_blank">Demo</a>
+                        <a href="https://github.com/gelbphoenix/gelbphoenix.github.io/tree/version-1.0.0.0" class="box-botton" target="_blank">Source Code</a>
+                    </div>
+                    <div class="content-box">
+                        <h4 style="margin-block-start: 0.5rem; margin-block-end: 0;">Diese Webpage (Version 2.0)</h4>
                         <img src="/frontend/index/favicon.png" style="width: 3rem;">
-                        <p style="margin-block-start: .2rem;">Diese Webpage wurde von mir zu 100% selber geschrieben und gestaltet.</p>
-                        <a href="https://github.com/gelbphoenix/gelbphoenix.github.io" class="box-botton">Source Code</a>
+                        <p style="margin-block-start: .2rem;">Wie auch Version 1.0 wurde diese Web&#173;page ebenfalls von mir zu 100% selber ge&#173;schrie&#173;ben. Und ist mein erster Versuch einer Single-Page-"App".</p>
+                        <a href="https://github.com/gelbphoenix/gelbphoenix.github.io/tree/version-2.0.0.0" class="box-botton" target="_blank">Source Code<a>
                     </div>
                 </div>
             </section>
