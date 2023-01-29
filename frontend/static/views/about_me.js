47488562f53ef7a2e1b3632d79154b50dffc3c3e
gelbphoenix <122753910+gelbphoenix@users.noreply.github.com>
2023-01-29 13:38:24 +0100

    fix

--- frontend/static/views/about_me.js
+++ frontend/static/views/about_me.js
@@ -17,8 +17,7 @@ export default class extends AbstractView {
                 <div class="content-bar">
                     <div class="content-box">
                         <h4>Politik:</h4>
-                        <span id="button-pol" class="box-botton">Infos:</span>
-                        <div id="politic-infos" style="display: none; flex-direction: column;">
+                        <div id="politic-infos" style="flex-direction: column;">
                             <p>
                                 Ich bin 2019 der FDP und den Jungen Liberalen beigetreten. 2019 wurde ich ebenfalls auch Beisitzerin im Bezirks&#173;vorstand der Julis OWL, welches ich bis 2020 ausgeübt habe.<br><br>
                                 Nach der Bundestagswahl 2021 bin ich der liberalen queer&#173;politischen Vorfelds&#173;organisa&#173;tion LiSL beigetreten und wurde 2022 in den Landesvorstand von LiSL NRW gewählt.
@@ -27,8 +26,7 @@ export default class extends AbstractView {
                     </div>                    
                     <div class="content-box">
                         <h4>Coding:</h4>
-                        <span id="button-cod" class="box-botton">Infos:</span>
-                        <div id="code-infos" class="code-style" style="display: none; flex-direction: column;">
+                        <div id="code-infos" class="code-style" style="display:flex; flex-direction: column;">
                             <p>
                                 In der Vergangenheit habe ich ein bisschen mich in der Scriptsprache Batch ausprobiert, allerdings schnell wieder an der Aktivität die Motivation verloren. Ende 2022/Anfang 2023 dann, habe ich das Interesse am Programmieren gefunden und angefangen dies zu lernen.<br><br>
                                 Aktuell wurden von mir mit HTML, CSS und Javascript nur einige private Browser&#173;erweiter&#173;ungen und Webpages gecoded.
@@ -40,8 +38,7 @@ export default class extends AbstractView {
                     </div>                    
                     <div class="content-box">
                         <h4>Generell:</h4>
-                        <span id="button-gen" class="box-botton">Infos:</span>
-                        <div id="general-infos" style="display: none; padding-left: 5px; padding-right: 5px;">
+                        <div id="general-infos" style="padding-left: 5px; padding-right: 5px;">
                             <p>
                                 2001 wurde ich in der Stadt Essen im Ruhrgebiet geboren. 2007 dann, zog ich als 5 Jährige nach Ostwestfalen-Lippe in die Stadt Schloß Holte-Stukenbrock, in der ich auch meinen Realschulabschluss gemacht habe.<br>
                                 2021 zog ich dann nach Rheda-Wiedenbrück.<br>
